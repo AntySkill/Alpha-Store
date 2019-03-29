750c9f2ac4d983a1a01dab693e18a3862eca0eb8
AntySkill <sebastian.juras@gmail.com>
2019-03-29 22:12:22 +0100

    Created basic components and added routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HENLO Navbar</h2>
+      </div>
+    )
+  }
+}
